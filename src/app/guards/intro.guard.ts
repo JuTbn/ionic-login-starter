a8fd51d90fb13f116b5993cb50ab98c35fcdea2e
JuTbn <jtoubon96@gmail.com>
2022-04-25 14:46:17 +0100

    creating the introduction page

--- src/app/guards/intro.guard.ts
+++ src/app/guards/intro.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanLoad, Router } from '@angular/router';
+import { Storage } from '@capacitor/storage';
+
+export const INTRO_KEY = 'intro-seen';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +12,15 @@ import { Observable } from 'rxjs';
  * Check if the user has already seen the intro and show the page if not
  */
 export class IntroGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(private router: Router) {}
+
+  async canLoad(): Promise<boolean> {
+    const hasSeenIntro = await Storage.get({key : INTRO_KEY});
+    if (hasSeenIntro && (hasSeenIntro.value === 'true')) {
+      return true;
+    }else {
+      this.router.navigateByUrl('/intro', {replaceUrl: true});
+    }
   }
 }
